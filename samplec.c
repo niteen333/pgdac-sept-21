f9b7646a5fd9d808151f414d94d22bd840ccf86c
niteen333 <nitinpawar333np@gmail.com>
2021-10-06 15:03:48 +0530

    main is modified

--- samplec.c
+++ samplec.c
@@ -4,10 +4,11 @@ void addtwo()
     int n1,n2;
     printf ("enter two number ");
     scanf ("%d,%d,&n1,&n2);
-    printf (sum of two num is %d", n1+n2);
+    printf (sum of is  two num is %d", n1+n2);
 }
 {
  void main ()
+ addtwo();
 }
 
 
