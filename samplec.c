ce80c0694a3e588713428bbfb28bc48dad470a3e
niteen333 <nitinpawar333np@gmail.com>
2021-10-06 14:57:55 +0530

    first copy my file

---
+++ samplec.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void addtwo()
+{
+    int n1,n2;
+    printf ("enter two number ");
+    scanf ("%d,%d,&n1,&n2);
+    printf (sum of two num is %d", n1+n2);
+}
+{
+ void main ()
+}
+
+
